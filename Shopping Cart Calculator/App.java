c1b8b730c706de56a99394e619d6febe836e8a58
RattaphumFame <rattaphum.s@ku.th>
2025-07-13 18:20:54 +0700

    Add files via upload

---
+++ Shopping Cart Calculator/App.java
@@ -0,0 +1,8 @@
+
+import LIb.ShoppingCartManualTest;
+
+public class App {
+    public static void main(String[] args) {
+        ShoppingCartManualTest.run();
+    }
+}
---
+++ Shopping Cart Calculator/LIb/CartItem.java
@@ -0,0 +1,4 @@
+package LIb;
+public record CartItem(String sku, String name, double price, int quantity){
+
+}
